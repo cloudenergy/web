26fd513be54ba3dc0531141f851917c73a281823
realm <realm.x@hotmail.com>
2016-03-10 15:41:07 +0800

    update v3.1.0

---
+++ dist/js/directives/auto-height.js
@@ -0,0 +1,1 @@
+EMAPP.register.directive("autoHeight",["$timeout",function(t){return{restrict:"A",scope:{height:"=*autoHeight"},link:function(e,i,n,o){var r=function(){i.height($(window).innerHeight()-i.offset().top-(e.height?parseInt(e.height):15))};r(),t(r),$(window).on("resize",r),e.$watch(function(){return e.height},r),e.$on("$destroy",function(){$(window).off("resize",r)})}}}]);
