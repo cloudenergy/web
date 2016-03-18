974f42b8941a68eeecd5a344eead69f4206efdb0
realm <realm.x@hotmail.com>
2016-03-18 15:09:51 +0800

    usage v3.1.3

---
+++ dist/js/directives/flatui-switch.js
@@ -0,0 +1,1 @@
+EMAPP.register.directive("flatuiSwitch",["$timeout",function(t){return{restrict:"A",link:function(i,n,c,o){t(function(){n.bootstrapSwitch()}),i.$watch(c.flatuiSwitchChange,function(i){i&&n.on("switchChange.bootstrapSwitch",function(){t($.proxy(function(){i.apply(i,this)},arguments))})})}}}]);
