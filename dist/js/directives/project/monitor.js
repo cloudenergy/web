8de06cd3e6d12aa0e477c31168d3a71da93a9b7b
realm <realm.x@hotmail.com>
2016-06-22 05:43:35 +0800

    usage v3.2.2

--- dist/js/directives/project/monitor.js
+++ dist/js/directives/project/monitor.js
@@ -1,1 +1,1 @@
-EMAPP.register.directive("monitorSelect",function(){return{restrict:"A",link:function(e,t,n,i){t.select2({dropdownCssClass:"monitor-select2-dropdown-inverse"}).change(function(){e.self.level=this.value,e.self.list()})}}});
+angular.module("EMAPP").directive("monitorSelect",function(){return{restrict:"A",link:function(e,n,t,l){n.select2({dropdownCssClass:"monitor-select2-dropdown-inverse"}).change(function(){e.self.level=this.value,e.self.list()})}}});
