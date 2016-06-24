ab95a41e353a65cb5a8c5402f1d69a74467f46f0
Realm <realm.x@hotmail.com>
2016-06-24 13:00:03 +0800

    usage v3.3.0

--- dist/js/directives/slimscroll.js
+++
@@ -1,1 +0,0 @@
-angular.module("EMAPP").directive("slimscroll",["$q","$ocLazyLoad",function(l,r){var o=[r.load("/vendor/slimScroll/jquery.slimscroll.min.js?rev=b35cf0acb6")];return{restrict:"A",link:function(r,c,i,n){var e=l.defer(),s={};r.$watch(i.slimscroll,function(l){e.resolve(s=l||{})}),l.all(o.concat([e.promise])).then(function(){c.slimScroll(s)})}}}]);
